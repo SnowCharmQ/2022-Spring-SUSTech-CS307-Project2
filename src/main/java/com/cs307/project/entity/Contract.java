c8080cb38ba99c41c978105531357e5ee7e218b7
leopold-lee <12012705@mail.sustech.edu.cn>
2022-05-09 22:46:17 +0800

    add method for api 13, alter table contract and the trigger

---
+++ src/main/java/com/cs307/project/entity/Contract.java
@@ -0,0 +1,19 @@
+package com.cs307.project.entity;
+
+import java.util.List;
+
+public class Contract {
+    String contract_num, contract_manager, enterprise;
+
+    public String getContract_num() {
+        return contract_num;
+    }
+
+    public String getContract_manager() {
+        return contract_manager;
+    }
+
+    public String getEnterprise() {
+        return enterprise;
+    }
+}
