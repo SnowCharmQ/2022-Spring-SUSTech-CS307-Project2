e0ffa1bb42e1f3fb311d541f1cc012bce2842085
leopold-lee <12012705@mail.sustech.edu.cn>
2022-05-17 14:06:18 +0800

    add redis cache

--- src/main/java/com/cs307/project/service/impl/IServiceImpl.java
+++ src/main/java/com/cs307/project/service/impl/IServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -32,6 +33,8 @@ public class IServiceImpl implements IService {
 
     @Override
     public void stockIn(StockIn stockIn) {
+        stockChanged = true;
+        product_cache.remove(stockIn.getProductModel());
         String supplyCenter = stockIn.getSupplyCenter();
         Center center = selectMapper.selectCenterByName(supplyCenter);
         if (center == null) throw new SupplyCenterNotFoundException("Supply center does not exist");
@@ -56,6 +59,11 @@ public class IServiceImpl implements IService {
     @Override
     public void placeOrder(PlaceOrder placeOrder) {
         orderChanged = true;
+        contractChanged = true;
+        stockChanged = true;
+        productChanged = true;
+        contract_cache.remove(placeOrder.getContractNum());
+
         Enterprise enterprise = selectMapper.selectEnterpriseByName(placeOrder.getEnterprise());
         String supplyCenter = enterprise.getSupplyCenter();
         Integer stock = selectMapper.selectStockByModel(supplyCenter, placeOrder.getProductModel());
@@ -73,6 +81,11 @@ public class IServiceImpl implements IService {
     @Override
     public void updateOrder(String contractNum, String productModel, String salesmanNum, int quantity, Date estimatedDeliveryDate, Date lodgementDate) {
         orderChanged = true;
+        productChanged = true;
+        favoriteChanged = true;
+        stockChanged = true;
+        contract_cache.remove(contractNum);
+
         List<PlaceOrder> list = selectMapper.selectOrderBySalesman(salesmanNum);
         PlaceOrder placeOrder = null;
         for (PlaceOrder po : list) {
@@ -96,6 +109,11 @@ public class IServiceImpl implements IService {
     @Override
     public void deleteOrder(String contract, String salesman, int seq) {
         orderChanged = true;
+        productChanged = true;
+        stockChanged = true;
+        favoriteChanged = true;
+        contract_cache.remove(contract);
+
         List<PlaceOrder> orders = selectMapper.selectOrderByContractNum(contract, salesman);
         if (orders == null || orders.size() < seq) throw new OrderNotFoundException("No order of the seq");
         PlaceOrder order = orders.get(seq - 1);
@@ -109,6 +127,7 @@ public class IServiceImpl implements IService {
     @Autowired
     private RedisService redisService;
 
+    boolean staffChanged = true;
     public static final String STAFF_COUNT = "staff_count";
 
     @Override
@@ -121,18 +140,19 @@ public class IServiceImpl implements IService {
             System.out.println("connection to redis failed");
         }
         List<StaffCount> staffCounts;
-        if (staffCountString == null) {
+        if (staffCountString == null || staffChanged) {
             synchronized (this) {
                 try {
                     staffCountString = redisService.get(STAFF_COUNT);
                 } catch (Exception e) {
                     System.out.println("connection to redis failed");
                 }
-                if (staffCountString == null) {
+                if (staffCountString == null || staffChanged) {
                     System.out.println("============from database===========");
                     staffCounts = selectMapper.selectAllStaffCount();
                     String jsonString = JSON.toJSONString(staffCounts);
                     redisService.set(STAFF_COUNT, jsonString);
+                    staffChanged = false;
                     redisService.expire(STAFF_COUNT, 500);
                 } else staffCounts = JSONArray.parseArray(staffCountString, StaffCount.class);
             }
@@ -145,6 +165,7 @@ public class IServiceImpl implements IService {
 
     boolean orderChanged = true;
     public static final String ORDER_COUNT = "order_count";
+
     @Override
     public Integer getOrderCount() {
         //return selectMapper.selectOrderCount();
@@ -162,7 +183,7 @@ public class IServiceImpl implements IService {
                 } catch (Exception e) {
                     System.out.println("connection to redis failed");
                 }
-                if (order == null) {
+                if (order == null || orderChanged) {
                     System.out.println("============from database===========");
                     orderCount = selectMapper.selectOrderCount();
                     String jsonString = JSON.toJSONString(orderCount);
@@ -197,7 +218,7 @@ public class IServiceImpl implements IService {
                 } catch (Exception e) {
                     System.out.println("connection to redis failed");
                 }
-                if (contract == null) {
+                if (contract == null || contractChanged) {
                     System.out.println("============from database===========");
                     contractCount = selectMapper.selectContractCount();
                     String jsonString = JSON.toJSONString(contractCount);
@@ -214,12 +235,40 @@ public class IServiceImpl implements IService {
     }
 
     boolean productChanged = true;
+    public static final String NEVER_SOLD_PRODUCT_COUNT = "never_sold_count";
+
     public Integer getNeverSoldProductCount() {
-        return selectMapper.getNeverSoldProductCount();
+        String never = null;
+        int neverSoldCount;
+        try {
+            never = redisService.get(NEVER_SOLD_PRODUCT_COUNT);
+        } catch (Exception e) {
+            System.out.println("connection to redis failed");
+        }
+        if (never == null || productChanged) {
+            synchronized (this) {
+                try {
+                    never = redisService.get(NEVER_SOLD_PRODUCT_COUNT);
+                } catch (Exception e) {
+                    System.out.println("connection to redis failed");
+                }
+                if (never == null || productChanged) {
+                    System.out.println("=========from database==========");
+                    neverSoldCount = selectMapper.getNeverSoldProductCount();
+                    never = JSON.toJSONString(neverSoldCount);
+                    redisService.set(NEVER_SOLD_PRODUCT_COUNT, never);
+                    productChanged = false;
+                    redisService.expire(NEVER_SOLD_PRODUCT_COUNT, 500);
+                } else neverSoldCount = Integer.parseInt(never);
+            }
+        } else neverSoldCount = Integer.parseInt(never);
+        return neverSoldCount;
+        //return selectMapper.getNeverSoldProductCount();
     }
 
     boolean stockChanged = true;
     public static final String AVG_STOCK = "avg_stock";
+
     public List<AvgStockByCenter> getAvgStockByCenter() {
         List<AvgStockByCenter> avgStockByCenters;
         String avgStock = null;
@@ -251,41 +300,137 @@ public class IServiceImpl implements IService {
         return avgStockByCenters;
     }
 
+
+    boolean favoriteChanged = true;
+    public static final String FAVORITE_PRODUCT = "favorite_product";
+
     @Override
     public FavoriteModel getFavoriteProductModel() {
-        List<StockQuantity> stockQuantityList = selectMapper.selectStockQuantity();
-        List<OrderQuantity> orderQuantityList = selectMapper.selectOrderQuantity();
-        String productModel = null;
-        int max = 0;
-        for (StockQuantity sq : stockQuantityList) {
-            OrderQuantity oq = orderQuantityList.stream()
-                    .filter(o -> o.getProductModel().equals(sq.getProductModel()))
-                    .findFirst().get();
-            int sales = sq.getSum() - oq.getSum();
-            if (sales > max) {
-                max = sales;
-                productModel = oq.getProductModel();
-            }
+        String favorite = null;
+        FavoriteModel model;
+        try {
+            favorite = redisService.get(FAVORITE_PRODUCT);
+        } catch (Exception e) {
+            System.out.println("connection to redis failed");
         }
-        return new FavoriteModel(productModel, max);
+        if (favorite == null || favoriteChanged) {
+            synchronized (this) {
+                try {
+                    favorite = redisService.get(FAVORITE_PRODUCT);
+                } catch (Exception e) {
+                    System.out.println("connection to redis failed");
+                }
+                if (favorite == null || favoriteChanged) {
+                    System.out.println("============from database===========");
+                    List<StockQuantity> stockQuantityList = selectMapper.selectStockQuantity();
+                    List<OrderQuantity> orderQuantityList = selectMapper.selectOrderQuantity();
+                    String productModel = null;
+                    int max = 0;
+                    for (StockQuantity sq : stockQuantityList) {
+                        OrderQuantity oq = orderQuantityList.stream()
+                                .filter(o -> o.getProductModel().equals(sq.getProductModel()))
+                                .findFirst().get();
+                        int sales = sq.getSum() - oq.getSum();
+                        if (sales > max) {
+                            max = sales;
+                            productModel = oq.getProductModel();
+                        }
+                    }
+                    model = new FavoriteModel(productModel, max);
+                    favorite = JSON.toJSONString(model);
+                    redisService.set(FAVORITE_PRODUCT, favorite);
+                    favoriteChanged = false;
+                    redisService.expire(FAVORITE_PRODUCT, 500);
+                } else model = JSON.parseObject(favorite, FavoriteModel.class);
+            }
+        } else model = JSON.parseObject(favorite, FavoriteModel.class);
+        return model;
     }
+
+    HashSet<String> product_cache = new HashSet<>();
+    public static final String PRODUCT_BY_NUMBER = "product_by_number: ";
 
     @Override
     public List<ProductStock> getProductByNumber(String number) {
-        return selectMapper.selectProductByModel(number);
+        String product = null;
+        List<ProductStock> productStocks;
+        try {
+            product = redisService.get(PRODUCT_BY_NUMBER + number);
+        } catch (Exception e) {
+            System.out.println("connection to redis failed");
+        }
+        if (product == null || !product_cache.contains(number)) {
+            synchronized (this) {
+                try {
+                    product = redisService.get(PRODUCT_BY_NUMBER + number);
+                } catch (Exception e) {
+                    System.out.println("connection to redis failed");
+                }
+                if (product == null || !product_cache.contains(number)) {
+                    System.out.println("===========from database===========");
+                    productStocks = selectMapper.selectProductByModel(number);
+                    product = JSON.toJSONString(productStocks);
+                    redisService.set(PRODUCT_BY_NUMBER + number, product);
+                    product_cache.add(number);
+                    redisService.expire(PRODUCT_BY_NUMBER + number, 500);
+                } else productStocks = JSONArray.parseArray(product, ProductStock.class);
+            }
+        } else productStocks = JSONArray.parseArray(product, ProductStock.class);
+
+        return productStocks;
     }
+
+    HashSet<String> contract_cache = new HashSet<>();
+    public static final String CONTRACT_INFO = "contract_info: ";
 
     @Override
     public Contract getContractInfo(String contract_number) {
-        List<PlaceOrder> orders = selectMapper.selectContract(contract_number);
-        Contract contract = selectMapper.getContractInfo(contract_number);
-        List<Contract.OrderInContract> orderlists = orders.stream().map(new Function<PlaceOrder, Contract.OrderInContract>() {
-            @Override
-            public Contract.OrderInContract apply(PlaceOrder placeOrder) {
-                Contract.OrderInContract order = new Contract.OrderInContract(placeOrder.getProductModel(), selectMapper.selectStaffByNumber(placeOrder.getSalesmanNum()).getName(), placeOrder.getQuantity(), selectMapper.selectModelByModel(placeOrder.getProductModel()).getUnitPrice(), placeOrder.getEstimatedDeliveryDate(), placeOrder.getLodgementDate());
-                return order;
+        String info = null;
+        Contract contract;
+        try {
+            info = redisService.get(CONTRACT_INFO + contract_number);
+        } catch (Exception e) {
+            System.out.println("connection to redis failed");
+        }
+        if (info == null || !contract_cache.contains(CONTRACT_INFO)) {
+            synchronized (this) {
+                try {
+                    info = redisService.get(CONTRACT_INFO + contract_number);
+                } catch (Exception e) {
+                    System.out.println("connection to redis failed");
+                }
+                if (info == null || !contract_cache.contains(CONTRACT_INFO)) {
+                    System.out.println("========from database==============");
+                    List<PlaceOrder> orders = selectMapper.selectContract(contract_number);
+                    contract = selectMapper.getContractInfo(contract_number);
+                    List<Contract.OrderInContract> orderlists = orders.stream().map(new Function<PlaceOrder, Contract.OrderInContract>() {
+                        @Override
+                        public Contract.OrderInContract apply(PlaceOrder placeOrder) {
+                            Contract.OrderInContract order = new Contract.OrderInContract(placeOrder.getProductModel(), selectMapper.selectStaffByNumber(placeOrder.getSalesmanNum()).getName(), placeOrder.getQuantity(), selectMapper.selectModelByModel(placeOrder.getProductModel()).getUnitPrice(), placeOrder.getEstimatedDeliveryDate(), placeOrder.getLodgementDate());
+                            return order;
+                        }
+                    }).collect(Collectors.toList());
+                    contract = new Contract(contract_number, selectMapper.selectStaffByNumber(contract.getContract_manager()).getName(), contract.getEnterprise(), selectMapper.selectEnterpriseByName(contract.getEnterprise()).getSupplyCenter(), orderlists);
+                    info = JSON.toJSONString(contract);
+                    redisService.set(CONTRACT_INFO + contract_number, info);
+                    contract_cache.add(contract_number);
+                    redisService.expire(CONTRACT_INFO + contract_number, 500);
+                } else contract = JSON.parseObject(info, Contract.class);
             }
-        }).collect(Collectors.toList());
-        return new Contract(contract_number, selectMapper.selectStaffByNumber(contract.getContract_manager()).getName(), contract.getEnterprise(), selectMapper.selectEnterpriseByName(contract.getEnterprise()).getSupplyCenter(), orderlists);
+        } else contract = JSON.parseObject(info, Contract.class);
+
+        return contract;
+
+
+//        List<PlaceOrder> orders = selectMapper.selectContract(contract_number);
+//        Contract contract = selectMapper.getContractInfo(contract_number);
+//        List<Contract.OrderInContract> orderlists = orders.stream().map(new Function<PlaceOrder, Contract.OrderInContract>() {
+//            @Override
+//            public Contract.OrderInContract apply(PlaceOrder placeOrder) {
+//                Contract.OrderInContract order = new Contract.OrderInContract(placeOrder.getProductModel(), selectMapper.selectStaffByNumber(placeOrder.getSalesmanNum()).getName(), placeOrder.getQuantity(), selectMapper.selectModelByModel(placeOrder.getProductModel()).getUnitPrice(), placeOrder.getEstimatedDeliveryDate(), placeOrder.getLodgementDate());
+//                return order;
+//            }
+//        }).collect(Collectors.toList());
+//        return new Contract(contract_number, selectMapper.selectStaffByNumber(contract.getContract_manager()).getName(), contract.getEnterprise(), selectMapper.selectEnterpriseByName(contract.getEnterprise()).getSupplyCenter(), orderlists);
     }
 }
--- src/test/java/com/cs307/project/CacheDemoTests.java
+++ src/test/java/com/cs307/project/CacheDemoTests.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -17,51 +15,8 @@ public class CacheDemoTests{
     @Autowired
     private IService iService;
 
-//    @Autowired
-//    private RedisServiceImpl redisService;
-//
-//    @Test
-//    public void QueryTest()
-//    {
-//        iService.getContractCount();
-//        iService.getContractCount();
-//        iService.getContractCount();
-//        iService.getContractCount();
-//        iService.getContractCount();
-//    }
-//
-//    @Test
-//    public void testRedisSet()
-//    {
-//        Boolean flag = redisService.set("gerry", "gerry-value");
-//        System.out.println(flag);
-//        flag = redisService.set("le", "gerry-value");
-//        redisService.expire("gerry", 5000);
-//    }
-//
-//    @Test
-//    public void concurrencyTest()
-//    {
-//        CountDownLatch countDownLatch = new CountDownLatch(100000);
-//        for (int i = 0; i < 100000; i++) {
-//            new Thread(() ->{
-//                countDownLatch.countDown();
-//                iService.getAvgStockByCenter();
-//            }).start();
-//        }
-//
-//        try {
-//            countDownLatch.await();
-//            //Thread.currentThread().join();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
     @Test
     public void getAllStaffCountTest() {//6
-//        List<StaffCount> staffCounts = iService.getAllStaffCount();
-//        for (StaffCount sc : staffCounts) System.out.println(sc);
         for (int i = 0; i < 100000; i++) {
             new Thread(() ->{
                 iService.getAllStaffCount();
@@ -71,18 +26,36 @@ public class CacheDemoTests{
 
     @Test
     public void getContractNumberTest() {//7
+        for (int i = 0; i < 100000; i++) {
+            new Thread(() ->{
+                iService.getContractCount();
+            }).start();
+        }
         Integer cnt = iService.getContractCount();
         System.out.println(cnt);
     }
 
     @Test
     public void getOrderCountTest() {//8
-//        Integer cnt = iService.getOrderCount();
-//        System.out.println(cnt);
         for (int i = 0; i < 100000; i++) {
             new Thread(() ->{
                 iService.getOrderCount();
             }).start();
         }
     }
+
+    @Test
+    public void ConcurrencyTest() {//8
+        for (int i = 0; i < 1000; i++) {
+            new Thread(() ->{
+                iService.getAllStaffCount();
+                iService.getContractCount();
+                iService.getOrderCount();
+                iService.getAvgStockByCenter();
+                iService.getProductByNumber("A50L172");
+                iService.getContractInfo("CSE0000106");
+            }).start();
+        }
+    }
+
 }
